f32268f941cce86efeb57ff995933f6dbb41427a
highstar97 <pgt0185@gmail.com>
2025-02-05 18:46:56 +0900

    [Weapon] 공격 매커니즘 변경
    * 기존 : 캐릭터가 바라보는 방향 -> 변경 : 마우스 포인터의 위치를 바라보며

--- Source/US/Weapons/USGun.cpp
+++ Source/US/Weapons/USGun.cpp
@@ -21,29 +21,47 @@ void AUSGun::Attack()
 {
 	Super::Attack();
 
-	if (!ensure(WeaponDataAsset != nullptr)) return;
+	if (!OwnerCharacter.IsValid()) return;
+	if (!IsValid(WeaponDataAsset)) return;
 
 	float InitDamage = WeaponDataAsset->NumericData.Damage;
-
 	UE_LOG(LogTemp, Warning, TEXT("%s Attack!"), *this->GetActorLabel());
 
-	FVector StartLocation = ArrowComponent->GetComponentLocation() + FVector(0.0f, 42.0f, 0.0f);
-	FVector ForwardVector = ArrowComponent->GetForwardVector();
-	const float Distance = 10000.0f;
-	FVector EndLocation = StartLocation + (ForwardVector * Distance);
+	APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+	if (!IsValid(PlayerController)) return;
+
+	float MouseX, MouseY;
+	if (!PlayerController->GetMousePosition(MouseX, MouseY)) return;
 
 	FHitResult HitResult;
+	if (!PlayerController->GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, true, HitResult)) return;
+
+	DrawDebugSphere(GetWorld(), HitResult.Location, 1.f, 12, FColor::Green, false, 1.0f);
+
+	FVector TargetLocation = HitResult.Location;
+	TargetLocation.Z = 0.f;
+
+	FVector MuzzleLocation = ArrowComponent->GetComponentLocation();
+	MuzzleLocation.Z = 0.f;
+	FVector DirectionToTarget = TargetLocation - MuzzleLocation;
+	
+	OwnerCharacter->SetActorRotation(DirectionToTarget.Rotation());
+
+	// Ray Trace
+	FVector TraceStart = ArrowComponent->GetComponentLocation();
+	const float TraceDistance = 10000.0f;
+	FVector TraceEnd = TraceStart + (OwnerCharacter->GetActorForwardVector() * TraceDistance);
+	//FVector TraceEnd = TraceStart + (ArrowComponent->GetForwardVector() * TraceDistance);
+
+	HitResult.Reset();
 	FCollisionQueryParams QueryParams;
 	TArray<TWeakObjectPtr<const AActor>> IgnoredActors;
 	IgnoredActors.Add(this);
-	if (OwnerCharacter.IsValid())
-	{
-		IgnoredActors.Add(OwnerCharacter.Get());
-	}
+	IgnoredActors.Add(OwnerCharacter.Get());
 	QueryParams.AddIgnoredActors(IgnoredActors);
 
-	bool bHit = GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECC_GameTraceChannel3, QueryParams);
-	DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, false, 1.0f, 0, 2.0f);
+	bool bHit = GetWorld()->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, ECC_GameTraceChannel3, QueryParams);
+	DrawDebugLine(GetWorld(), TraceStart, TraceEnd, FColor::Red, false, 1.0f, 0, 2.0f);
 	if (bHit)
 	{
 		AActor* HitActor = HitResult.GetActor();
@@ -54,6 +72,7 @@ void AUSGun::Attack()
 			{
 				UE_LOG(LogTemp, Warning, TEXT("Hit Character: %s"), *HitCharacter->GetName());
 				DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, 10.0f, 12, FColor::Green, false, 1.0f);
+				// TODO : Check Damage Logic
 				UGameplayStatics::ApplyDamage(HitCharacter, InitDamage, GetInstigatorController(), this, UDamageType::StaticClass());
 			}
 		}
